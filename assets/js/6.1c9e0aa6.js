d98ed03b8bac111662e3b95c42e53314b1eb3c6b
Nguyen Quang Phu <nguyen.quang.phu@sun-asterisk.com>
2020-10-29 09:30:00 +0700

    deploy

---
+++ assets/js/6.1c9e0aa6.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{361:function(n,e,t){"use strict";t.r(e);var l=t(42),s=Object(l.a)({},(function(){var n=this.$createElement;return(this._self._c||n)("div",[this._v("Hello nha")])}),[],!1,null,null,null);e.default=s.exports}}]);
